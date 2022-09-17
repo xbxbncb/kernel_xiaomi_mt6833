6bd636cd0c966e1063fde71f2b96cd73c2fe612c
bengris32 <bengris32@protonmail.ch>
2022-09-17 18:29:42 +0100

    drivers: touchscreen: NT36672C: Implement double_tap node
    Change-Id: I9b727e261b54bc0957dd01d8e3ec489bcf74f68c
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- drivers/input/touchscreen/NT36672C/nt36xxx.c
+++ drivers/input/touchscreen/NT36672C/nt36xxx.c
@@ -45,6 +45,12 @@
 #include <linux/hardware_info.h>
 #include <linux/platform_data/spi-mt65xx.h>
 
+#if WAKEUP_GESTURE
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+#include <linux/input/tp_common.h>
+#endif
+#endif
+
 #if NVT_TOUCH_ESD_PROTECT
 static struct delayed_work nvt_esd_check_work;
 static struct workqueue_struct *nvt_esd_check_wq;
@@ -1152,6 +1158,33 @@ void nvt_ts_wakeup_gesture_report(uint8_t gesture_id, uint8_t *data)
 		input_sync(ts->input_dev);
 	}
 }
+
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+static ssize_t double_tap_show(struct kobject *kobj,
+							   struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d\n", ts->gesture_enabled);
+}
+
+static ssize_t double_tap_store(struct kobject *kobj,
+								struct kobj_attribute *attr, const char *buf,
+								size_t count)
+{
+	int rc, val;
+
+	rc = kstrtoint(buf, 10, &val);
+	if (rc)
+		return -EINVAL;
+
+	ts->gesture_enabled = !!val;
+	return count;
+}
+
+static struct tp_common_ops double_tap_ops = {
+	.show = double_tap_show,
+	.store = double_tap_store
+};
+#endif
 #endif
 
 /*******************************************************
@@ -2079,6 +2112,14 @@ static int32_t nvt_ts_probe(struct spi_device *client)
 	for (retry = 0; retry < (sizeof(gesture_key_array) / sizeof(gesture_key_array[0])); retry++) {
 		input_set_capability(ts->input_dev, EV_KEY, gesture_key_array[retry]);
 	}
+
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+	ret = tp_common_set_double_tap_ops(&double_tap_ops);
+	if (ret < 0) {
+		NVT_ERR("%s: Failed to create double_tap node err=%d\n",
+				__func__, ret);
+	}
+#endif
 #endif
 
 	sprintf(ts->phys, "input/ts");
